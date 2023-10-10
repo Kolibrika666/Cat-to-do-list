00e37b7104b470e966f0fb9c599c1af035cf88ed
Kolibrika666 <140741201+Kolibrika666@users.noreply.github.com>
2023-10-10 23:30:17 +0700

    init: clone node

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-const rootDoc = document.getElementById('root')
-const createBtn = document.getElementById('create')
-const note = document.createElement('article')
-const rezult = document.createElement('p')
-const divBtn = document.createElement('div')
-const checkBtn = document.createElement('button')
-const deleteBtn = document.createElement('button')
 
-checkBtn.className = 'check'
-deleteBtn.className = 'delete'
+const parent = document.getElementById('parent');
+const elem = parent.querySelector('article');
+const createBtn = document.getElementById('create');
 
-
-function CreateNote () {
-        const  userNote = prompt('Добавить дело', 'Дело');
-        rootDoc.append(note)
-        note.append(rezult,  divBtn)
-        divBtn.append(checkBtn, deleteBtn)
-        rezult.textContent = userNote
+function CreateElement () {
+        let userMessage = prompt('Введите ваше дело')
+            let clone = elem.cloneNode(true);
+            clone.children[0].textContent = userMessage
+            parent.appendChild(clone);
 }
 
-function DeleteBtnOnclick () {
-    rezult.className = "delete__text"
-}
-
-function CheckBtnOnclick () {
-    rezult.className = "check__text"
-}
-
-deleteBtn.onclick = DeleteBtnOnclick;
-checkBtn.onclick = CheckBtnOnclick;
-createBtn.onclick =  CreateNote ;
-
+createBtn.onclick = CreateElement;
 
