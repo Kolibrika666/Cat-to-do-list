8afff81e64681dcfd5691d881228e9a6846aebcd
Kolibrika666 <140741201+Kolibrika666@users.noreply.github.com>
2023-10-10 23:32:53 +0700

    init: clone node

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const parent = document.getElementById('parent');
 const elem = parent.querySelector('article');
 const createBtn = document.getElementById('create');
+const deleteBtn = document.querySelector('delete');
+const checkBtn = document.querySelector('check');
 
 function CreateElement () {
         let userMessage = prompt('Введите ваше дело')
@@ -10,5 +12,14 @@ function CreateElement () {
             parent.appendChild(clone);
 }
 
-createBtn.onclick = CreateElement;
+// function deleteElement () {
 
+// }
+
+// function checkElement () {
+
+// }
+
+createBtn.onclick = CreateElement;
+// deleteBtn.onclick = deleteElement;
+// checkBtn.onclick = checkElement;
