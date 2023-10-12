094091abd2870c5d8314b043e5ad94650e0f3d5f
Kolibrika666 <140741201+Kolibrika666@users.noreply.github.com>
2023-10-12 15:18:38 +0700

    init: psrent delete

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 
 const parent = document.getElementById('parent');
 const elem = parent.querySelector('article');
+const p = elem.querySelector('p');
 const createBtn = document.getElementById('create');
-const deleteBtn = document.getElementById('del');
-const checkBtn = document.getElementById('ch');
+
 
 
 function CreateElement () {
-            userMessage = prompt('Введите ваше дело')
-            let clone = elem.cloneNode(true);
-            clone.className = "clean__text";
-            clone.children[0].textContent = userMessage
-            parent.appendChild(clone);
+    
+    userMessage = prompt('Введите ваше дело', 'Моё дело')
+    if (userMessage != 'Моё дело'){
+        let clone = elem.cloneNode(true);
+        clone.style.display = "block";
+        clone.children[0].textContent = userMessage;
+        parent.appendChild(clone);
+    } 
 }
 
+function DeleteElem () {
+    elem.style.display = "none"
+}
+
+
 createBtn.onclick = CreateElement;
-deleteBtn.onclick = function() { 
-    elem.className = "delete__text";
-}
-checkBtn.onclick = function() { 
-    elem.className = "check__text";
-}
+elem.onclick =  DeleteElem;
+
